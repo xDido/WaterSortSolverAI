a4dd4d9f2abe159d577585d6ed55c3abb52c2f8a
Dido <adhaithaam@gmail.com>
2024-10-14 02:36:58 +0300

    TRIED TO IMPLEMENT BFS

--- src/code/Algorithms/BFS.java
+++ src/code/Algorithms/BFS.java
@@ -6,75 +6,72 @@ import code.WaterSortSearch;
 
 import java.util.ArrayDeque;
 import java.util.ArrayList;
-
-import static code.Tools.Bottle.printBottles;
+import java.util.HashSet;
+import java.util.Set;
 
 public class BFS {
     private Node node;
     private WaterSortSearch waterSortSearch;
-
+    private int nodeCount;
     public BFS(ArrayList<Bottle> intialState, WaterSortSearch waterSortSearch) {
         this.node = new Node(intialState, null, null, 0, 0);
         this.waterSortSearch = waterSortSearch;
     }
 
+
+
     public void search() {
         ArrayDeque<Node> children = new ArrayDeque<>();
         children.addLast(node);  // Add the initial node
-
+        Set<ArrayList<Bottle>> visitedStates = new HashSet<>();  // To track visited states
+        nodeCount++;
         while (!children.isEmpty()) {
+            System.out.println(children.size() + " nodes in the queue");
             Node currentNode = children.poll();  // Dequeue a node
             ArrayList<Bottle> currentNodeState = currentNode.getState();
-            int nextDepth=0;
+
+            // Check if the current state has been visited
+            if (visitedStates.contains(currentNodeState)) {
+                continue;  // Skip this state if it has already been visited
+            }
+
+            // Mark the current state as visited
+            visitedStates.add(currentNodeState);
+
+            // Check if goal is reached
             if (waterSortSearch.isGoal(currentNodeState)) {
-                // Goal found, print the solution
-                System.out.println("************** BFS Result ********************");
-                for (Bottle bottle : currentNodeState) {
-                    System.out.println(bottle.getLayers());
+                System.out.println("************** GOAL STATE REACHED ********************");
+                // Print the solution or process it as needed
+                return;
                 }
-                System.out.println("*************************************************");
-                System.out.println("Goal Found");
-                System.out.println("Depth: " + currentNode.getDepth());
-                break;
-            } else {
+
 
                 for (int i = 0; i < currentNodeState.size(); i++) {
                     for (int j = 0; j < currentNodeState.size(); j++) {
                         if (i != j) {
                             Bottle sourceBottle = currentNodeState.get(i);
                             Bottle destinationBottle = currentNodeState.get(j);
-                            System.out.println("currentNode.getDepth(): " + (currentNode.getDepth()));
-                            System.out.println("currentNode.getDepth() + 1: " + (currentNode.getDepth() + 1));
-
                             if (waterSortSearch.isPourable(sourceBottle, destinationBottle)) {
-                                System.out.println("************** BFS Pouring ********************");
-                                System.out.println("Pouring from " + i + " to " + j);
-                                waterSortSearch.Pour(sourceBottle, destinationBottle);
-                                ArrayList<Bottle> nextState = currentNodeState;
-                                System.out.println("************** QUEUE ********************  Queue Size: " + children.size());
-                                for (Node x : children) {
-                                    System.out.println("BFSPrinting Queue");
-                                    printBottles(x.getState());
-                                }
-                                System.out.println("*************************************************");
-                                printBottles(nextState);
-                                System.out.println("currentNode.getDepth(): " + (currentNode.getDepth()));
-                                System.out.println("currentNode.getDepth() + 1: " + (currentNode.getDepth() + 1));
-                                System.out.println("nextDepth: " + nextDepth);
-                                Node childNode = new Node(nextState, currentNode, "Pour", nextDepth, 0);
-                                System.out.println(childNode.getDepth() +" childNode.getDepth()");
+                                ArrayList<Bottle> nextState = deepCopyState(currentNodeState);
+                                waterSortSearch.Pour(nextState.get(i), nextState.get(j));
+                                Node childNode = new Node(nextState, currentNode, "Pour", currentNode.getDepth() + 1, 0);
                                 children.addLast(childNode);
-
                             }
                         }
-                        System.out.println("************** DONE WITH J LOOP********************"); }
-                    nextDepth = currentNode.getDepth() + 1;
-                    System.out.println("************** DONE WITH J LOOP********************"); }
+                    }
+                }
             }
+     }
+
+    private ArrayList<Bottle> deepCopyState(ArrayList<Bottle> state) {
+        ArrayList<Bottle> newState = new ArrayList<>();
+        for (Bottle bottle : state) {
+            newState.add(new Bottle(bottle.getCapacity()));
         }
+        return newState;
     }
 
-
-
-
 }
+
+
+
